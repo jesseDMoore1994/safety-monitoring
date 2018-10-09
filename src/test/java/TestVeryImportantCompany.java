7ecda6d3ebde0d91f33ef09a43bf6866a1c9445f
Jesse Moore <jaymen1994@hotmail.com>
2018-10-08 22:48:17 -0500

    removing gradel hidden files and adding additional tests for very important company

--- src/test/java/TestVeryImportantCompany.java
+++ src/test/java/TestVeryImportantCompany.java
@@ -15,6 +15,9 @@ import static org.mockito.Mockito.verify;
 class VeryImportantCompanyTest {
 
     private VeryImportantCompany vic = new VeryImportantCompany();
+    private ArrayList<String> expectedWindowReturns = new ArrayList<String>();
+    private ArrayList<String> expectedDoorReturns = new ArrayList<String>();
+
 
     private boolean stringInArr(String target, ArrayList<String> arr) {
         for ( String str : arr) {
@@ -24,30 +27,48 @@ class VeryImportantCompanyTest {
         }
         return false;
     }
+
+    public VeryImportantCompanyTest () {
+        expectedWindowReturns.add("Open.");
+        expectedWindowReturns.add("Half Open.");
+        expectedWindowReturns.add("Closed but not Locked.");
+        expectedWindowReturns.add("Closed and Locked.");
+        expectedDoorReturns.add("Open.");
+        expectedDoorReturns.add("Closed but not Locked.");
+        expectedDoorReturns.add("Closed and Locked.");
+    }
  
     @Test
     void testGetWindowStatusFromRoom() {
-        ArrayList<String> expectedReturns = new ArrayList<String>();
-        expectedReturns.add("Open.");
-        expectedReturns.add("Half Open.");
-        expectedReturns.add("Closed but not Locked.");
-        expectedReturns.add("Closed and Locked.");
-		assertTrue(stringInArr(vic.getWindowStatusFromRoom("VIP Room", 0), expectedReturns));
-		assertTrue(stringInArr(vic.getWindowStatusFromRoom("VIP Room", 1), expectedReturns));
+		assertTrue(stringInArr(vic.getWindowStatusFromRoom("VIP Room", 0), expectedWindowReturns));
+		assertTrue(stringInArr(vic.getWindowStatusFromRoom("VIP Room", 1), expectedWindowReturns));
 		assertEquals(vic.getWindowStatusFromRoom("VIP Room", 3), "Target window does not exist.");
 		assertEquals(vic.getWindowStatusFromRoom("Fake Room", 0), "Target room does not exist.");
     }
  
     @Test
     void testGetDoorStatusFromRoom() {
-        ArrayList<String> expectedReturns = new ArrayList<String>();
-        expectedReturns.add("Open.");
-        expectedReturns.add("Closed but not Locked.");
-        expectedReturns.add("Closed and Locked.");
-		assertTrue(stringInArr(vic.getDoorStatusFromRoom("VIP Room", "VIP Door"), expectedReturns));
-        assertTrue(stringInArr(vic.getDoorStatusFromRoom("Receptionist Room", "Entry Door"), expectedReturns));
-        assertTrue(stringInArr(vic.getDoorStatusFromRoom("Receptionist Room", "VIP Door"), expectedReturns));
+		assertTrue(stringInArr(vic.getDoorStatusFromRoom("VIP Room", "VIP Door"), expectedDoorReturns));
+        assertTrue(stringInArr(vic.getDoorStatusFromRoom("Receptionist Room", "Entry Door"), expectedDoorReturns));
+        assertTrue(stringInArr(vic.getDoorStatusFromRoom("Receptionist Room", "VIP Door"), expectedDoorReturns));
 		assertEquals(vic.getDoorStatusFromRoom("Fake Room", "VIP Door"), "Target room does not exist.");
 		assertEquals(vic.getDoorStatusFromRoom("VIP Room", "Fake Door"), "Target door does not exist.");
+    }
+ 
+    @Test
+    void testCheckVIPRoomBoundaries() {
+		assertTrue(stringInArr(vic.getDoorStatusFromRoom("VIP Room", "VIP Door"), expectedDoorReturns));
+		assertEquals(vic.getDoorStatusFromRoom("VIP Room", "Entry Door"), "Target door does not exist.");
+		assertTrue(stringInArr(vic.getWindowStatusFromRoom("VIP Room", 0), expectedWindowReturns));
+		assertTrue(stringInArr(vic.getWindowStatusFromRoom("VIP Room", 1), expectedWindowReturns));
+		assertEquals(vic.getWindowStatusFromRoom("VIP Room", 2), "Target window does not exist.");
+    }
+ 
+    @Test
+    void testCheckReceptionistRoomBoundaries() {
+		assertTrue(stringInArr(vic.getDoorStatusFromRoom("Receptionist Room", "VIP Door"), expectedDoorReturns));
+		assertTrue(stringInArr(vic.getDoorStatusFromRoom("Receptionist Room", "Entry Door"), expectedDoorReturns));
+		assertEquals(vic.getDoorStatusFromRoom("Receptionist Room", "Fake Door"), "Target door does not exist.");
+		assertEquals(vic.getWindowStatusFromRoom("Receptionist Room", 0), "Target window does not exist.");
     }
 }
